3e8018e393cdff843293b632ec51f846a5e8cc21
Nick Yuen <138299516+NerdyClimber6969@users.noreply.github.com>
2025-04-06 18:08:50 +0800

    Change the props of the UserCard component
    -the prop changed from userInfo to username, firstName, lastName, status
    and src to make the component props more intuitive.

--- src/components/cards/UserCard.jsx
+++ src/components/cards/UserCard.jsx
@@ -1,22 +1,22 @@
 import Card from './Card.jsx';
 
-function UserCard({ userInfo }) {
+function UserCard({ username, firstName, lastName, status, src }) {
     return (
         <Card className='user-card'>
             <Card.Image>
                 <div className='container user-image-container'>
-                    <img className='user-image' src={userInfo.imgUrl}/>
+                    <img className='user-image' src={src}/>
                 </div>
             </Card.Image>
             <Card.MainContent>
                 <div className='container user-info'>
-                    <h3 className='font-lg mb2'>{userInfo.username}</h3>
-                    <p className='font-sm mb2'>{userInfo.firstName + ' ' + userInfo.lastName}</p>
+                    <h3 className='font-lg mb2'>{username}</h3>
+                    <p className='font-sm mb2'>{firstName + ' ' + lastName}</p>
                 </div>
             </Card.MainContent>
             <Card.Attribution>
                 <div className='container user-status font-xs'>
-                    <p>{userInfo.status}</p>
+                    <p>{status}</p>
                 </div>
             </Card.Attribution>
         </Card>
